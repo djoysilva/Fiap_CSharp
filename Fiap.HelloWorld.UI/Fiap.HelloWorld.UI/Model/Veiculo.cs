59a5f23f57f4639ffd7a32df546a695f2a421223
logonrm <logonrm@fiap.com.br>
2017-08-02 20:52:44 -0300

    Creating class with inheritance

---
+++ Fiap.HelloWorld.UI/Fiap.HelloWorld.UI/Model/Veiculo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.HelloWorld.UI.Model
+{
+    class Veiculo
+    {
+        //Field(Atributo)
+        private String _modelo;
+
+        //Gets e Sets
+        public string Modelo
+        {
+            get { return _modelo; }
+            set { _modelo = value; }
+        }
+    }
+}
---
+++ Fiap.HelloWorld.UI/Fiap.HelloWorld.UI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fiap.HelloWorld.UI.Model;
+using System.Collections.Generic;
+
+
+namespace Fiap.HelloWorld.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciar um carro
+            Carro carro = new Carro();
+            carro.Modelo = "Fusca";
+            carro.Placa = "JOY-9000";
+
+            //imprime o modelo
+            Console.WriteLine("Carro: " + carro.Modelo +  "\nPlaca: " + carro.Placa);
+            Console.ReadLine();
+        }
+    }
+}
